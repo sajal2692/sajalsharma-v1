f4c27011d9bce5cd9d557b2c11738ec479ec9af3
Sajal Sharma <sajal2692@gmail.com>
2017-05-29 17:57:35 +0530

    grunt init

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    watch: {
+
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'public/typed.min.js': ['public/typed.js'],
+        }
+      }
+    },
+    
+    cssmin: {
+
+    },
+    
+    htmlmin: {
+
+    }
+    
+  });
+
+  // Load npms
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-jekyll');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify','htmlmin','cssmin']);
+
+};
